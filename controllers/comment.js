2e97293ae032d2d4c9a105fc462bc768e0cb6ab4
Glitch (hsbookingbackend) <none>
2019-05-31 14:23:31 +0000

    👠🌯 Checkpoint ./controllers/comment.js:3622401/126 ./routes/comments.js:3622401/48

--- controllers/comment.js
+++ controllers/comment.js
@@ -3,9 +3,12 @@ const Booking = require('../models/booking');
 
 const Rental = require('../models/rental');
 const User = require('../models/user');
+const Comment = require('../models/comment')
 
 const { normalizeErrors } = require('../helpers/mongoose');
 
 exports.postComment = (req,res) => {
+  const user = res.locals.user;
+  
   
 }

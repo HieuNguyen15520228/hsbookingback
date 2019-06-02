cef9a26d74c7e27d5459de9493c3837f7babeefc
Glitch (hsbookingbackend) <none>
2019-06-02 04:23:17 +0000

    👙📟 Checkpoint ./routes/rentals.js:3622401/8554 ./controllers/rental.js:3622401/1681

--- controllers/rental.js
+++ controllers/rental.js
@@ -26,5 +26,52 @@ exports.getUserRentals = (req, res) => {
 }
 
 exports.verifyUser = (req, res) => {
-  
+  const user = res.locals.user;
+
+    Rental
+        .findById(req.params.id)
+        .populate('user')
+        .exec(function (err, foundRental) {
+            if (err) {
+                return res.status(422).send({ errors: normalizeErrors(err.errors) });
+            }
+
+            if (foundRental.user.id !== user.id) {
+                return res.status(422).send({ errors: { title: 'Invalid User!', detail: 'You are not rental owner!' } });
+            }
+
+
+            return res.json({ status: 'verified' });
+        });
+}
+
+exports.addSearchHistory = (req, res) => {
+  const rentalId = req.params.id;
+    const user= res.locals.user;
+    Rental.findById(rentalId)
+        .populate('user', 'username _id image message')
+        .populate('bookings', 'startAt endAt -_id')
+        .exec(function (err, foundRental) {
+
+            if (err || !foundRental) {
+                return res.status(422).send({ errors: [{ title: 'Rental Error!', detail: 'Could not find Rental!' }] });
+            }
+            if(user){
+              const searchHistory = user.searchHistory;
+              for (var i = 0; i < searchHistory.length; i++) {
+                if (String(searchHistory[i]._id) === rentalId) {
+                  searchHistory.splice(i, 1);
+                }
+              }
+              if(rentalId)
+                searchHistory.unshift(rentalId)
+              user.searchHistory = searchHistory;
+              console.log(searchHistory);
+              user.save((err) => {
+                if (err) {
+                  return res.status(422).send({ errors: normalizeErrors(err.errors) });
+              }})
+            }
+            return res.json(foundRental);
+        });
 }
--- routes/rentals.js
+++ routes/rentals.js
@@ -19,69 +19,16 @@ router.get('/secret', UserCtrl.authMiddleware, function (req, res) {
 
 router.get('/manage', UserCtrl.authMiddleware, RentalCtrl.getUserRentals);
 
-router.get('/:id/verify-user', UserCtrl.authMiddleware, function (req, res) {
-    const user = res.locals.user;
+router.get('/:id/verify-user', UserCtrl.authMiddleware, RentalCtrl.verifyUser);
 
-    Rental
-        .findById(req.params.id)
-        .populate('user')
-        .exec(function (err, foundRental) {
-            if (err) {
-                return res.status(422).send({ errors: normalizeErrors(err.errors) });
-            }
+router.get('/:id', UserCtrl.authOrNot ,RentalCtrl.addSearchHistory);
 
-            if (foundRental.user.id !== user.id) {
-                return res.status(422).send({ errors: { title: 'Invalid User!', detail: 'You are not rental owner!' } });
-            }
-
-
-            return res.json({ status: 'verified' });
-        });
-});
-
-router.get('/:id', UserCtrl.authOrNot ,(req, res) => {
-    const rentalId = req.params.id;
-    const user= res.locals.user;
-    Rental.findById(rentalId)
-        .populate('user', 'username _id image message')
-        .populate('bookings', 'startAt endAt -_id')
-        .exec(function (err, foundRental) {
-
-            if (err || !foundRental) {
-                return res.status(422).send({ errors: [{ title: 'Rental Error!', detail: 'Could not find Rental!' }] });
-            }
-            if(user){
-              const searchHistory = user.searchHistory;
-              for (var i = 0; i < searchHistory.length; i++) {
-                if (String(searchHistory[i]._id) === rentalId) {
-                  searchHistory.splice(i, 1);
-                }
-              }
-              if(rentalId)
-                searchHistory.unshift(rentalId)
-              user.searchHistory = searchHistory;
-              console.log(searchHistory);
-              user.save((err) => {
-                if (err) {
-                  return res.status(422).send({ errors: normalizeErrors(err.errors) });
-              }})
-            }
-            return res.json(foundRental);
-        });
-});
-
-router.get('/find/search', (req, res) => {
-    console.log(req.query);
-})
-
-
-router.get('/search/top', function (req, res) {
+router.get('/search/top', (req, res) => {
     Rental
         .find({})
-
         .sort({ rating: -1 })
-        .limit(5)
-        .select('-image -description')
+        .limit(25)
+        .select('id image title address rating price')
         .exec(function (err, foundRental) {
             if (err) {
                 return res.status(422).send({ errors: normalizeErrors(err.errors) });

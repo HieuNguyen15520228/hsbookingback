9fbc60fc474e3b47d31e5ef92bffd6f4987c3d03
Glitch (hsbookingbackend) <none>
2019-06-04 14:07:21 +0000

    🍥👾 Checkpoint ./routes/rentals.js:3622401/1869 ./controllers/rental.js:3622401/1920

--- controllers/rental.js
+++ controllers/rental.js
@@ -6,6 +6,7 @@ const ObjectId = mongoose.Types.ObjectId;
 const Rental = require('../models/rental');
 const User = require('../models/user');
 const Comment = require('../models/comment')
+const cloudinary = require('cloudinary')
 
 const { normalizeErrors } = require('../helpers/mongoose');
 
@@ -74,4 +75,43 @@ exports.addSearchHistory = (req, res) => {
             }
             return res.json(foundRental);
         });
+}
+exports.createRental = (req, res) => {
+    // const dUri = new Datauri();
+    Rental.findOne({ title: req.body.title }, (err, foundRental) => {
+        if (err)
+            return res.status(422).send({ errors: normalizeErrors(err.errors) });
+        if (foundRental)
+            return res.status(422).send({ errors: { title: 'Rentals Found!', detail: `Tên nhà ở đã có người sử dụng` } });
+        const { title, city, address, category, bedrooms, bathrooms, description, price, people, isTv, isWifi,
+            isElevator, isWashing, isFridge, isConditioner, image } = req.body;
+        const change = image.map(i => {
+            if (i.includes(',')) {
+                return cloudinary.uploader.upload(i)
+                    .then(result =>
+                        image[image.indexOf(i)] = result.url.slice(0, 45) + "q_auto:low/" + result.url.slice(45)
+                    )
+
+            }
+        })
+        Promise.all(change)
+            .then(() => {
+                const rental = new Rental({
+                    title, city, address, category, bedrooms, bathrooms,
+                    description, price, people, isTv, isWifi, isElevator, isWashing, isFridge,
+                    isConditioner, image
+                });
+                const user = res.locals.user;
+                rental.user = user;
+                Rental.create(rental, function (err, newRental) {
+                    if (err) {
+                        console.log(err)
+                        return res.status(422).send({ errors: normalizeErrors(err.errors) });
+                    }
+                    User.update({ _id: user.id }, { $push: { rentals: newRental } }, function () { });
+                    return res.json(newRental);
+                });
+            })
+            .catch((err) => { return res.status(422).send({ errors: normalizeErrors(err.errors) }); })
+    });
 }
--- routes/rentals.js
+++ routes/rentals.js
@@ -98,45 +98,7 @@ router.delete('/:id', UserCtrl.authMiddleware, function (req, res) {
         });
 });
 
-router.post('/create', UserCtrl.authMiddleware, multerUpload.multerUploads2, function (req, res) {
-    // const dUri = new Datauri();
-    Rental.findOne({ title: req.body.title }, (err, foundRental) => {
-        if (err)
-            return res.status(422).send({ errors: normalizeErrors(err.errors) });
-        if (foundRental)
-            return res.status(422).send({ errors: [{ title: 'Rentals Found!', detail: `Tên đã có người sử dụng` }] });
-        const { title, city, address, category, bedrooms, bathrooms, description, price, people, isTv, isWifi,
-            isElevator, isWashing, isFridge, isConditioner, image } = req.body;
-        const change = image.map(i => {
-            if (i.includes(',')) {
-                return cloudinary.uploader.upload(i)
-                    .then(result =>
-                        image[image.indexOf(i)] = result.url.slice(0, 45) + "q_auto:low/" + result.url.slice(45)
-                    )
-
-            }
-        })
-        Promise.all(change)
-            .then(() => {
-                const rental = new Rental({
-                    title, city, address, category, bedrooms, bathrooms,
-                    description, price, people, isTv, isWifi, isElevator, isWashing, isFridge,
-                    isConditioner, image
-                });
-                const user = res.locals.user;
-                rental.user = user;
-                Rental.create(rental, function (err, newRental) {
-                    if (err) {
-                        console.log(err)
-                        return res.status(422).send({ errors: normalizeErrors(err.errors) });
-                    }
-                    User.update({ _id: user.id }, { $push: { rentals: newRental } }, function () { });
-                    return res.json(newRental);
-                });
-            })
-            .catch((err) => { return res.status(422).send({ errors: normalizeErrors(err.errors) }); })
-    });
-})
+router.post('/create', UserCtrl.authMiddleware, multerUpload.multerUploads2, RentalCtrl.createRental )
 router.get('', function (req, res) {
     const city = req.query.city;
     const query = city ? { city: city.toLowerCase() } : {};

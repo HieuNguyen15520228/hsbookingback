4b7a561bb30ac635ddb3189cf51a6ad4d53b597e
Glitch (hsbookingbackend) <none>
2019-06-05 02:26:35 +0000

    🌗🌕 Checkpoint ./controllers/admin-user.js:3622401/11596 ./routes/admin:3622401/51

--- controllers/admin-user.js
+++ controllers/admin-user.js
@@ -30,43 +30,21 @@ exports.getUser = (req, res) => {
   // else
   //   return res.status(422).send({ errors: normalizeErrors(err.errors) });
 }
-
-exports.changeAvatar = (req, res) => {
-  const user = res.locals.user;
-  let { _id, email, oldImages } = res.locals.user;
-  if (req.file) {
-    const file = multerUpload.dataUri(req).content;
-    return cloudinary.v2.uploader.upload(file, {
-      folder: _id,
-      use_filename: true
-    })
-      .then((result) => {
-        oldImages.push(user.image);
-        const image = result.url.slice(0, 45) + "q_auto:low/" + result.url.slice(45);
-        User.findOneAndUpdate({ email }, { oldImages, image }, { new: true }, (err, user) => {
-          if (err)
-            return res.status(422).send({ errors: normalizeErrors(err.errors) });
-          if (user)
-            return res.status(200).json(user)
-        })
+exports.getAllUser = (req, res) => {
+    User.find()
+      .populate("searchHistory",'image title address _id')
+      .populate('rentals bookings')
+      .exec((err, foundUser) => {
+        if (err) {
+          return res.status(422).send({ errors: normalizeErrors(err.errors) });
+        }
+        return res.json(foundUser);
       })
-      .catch((err) => { return res.status(400).send({ errros: normalizeErrors(err.err) }) })
-  }
+  
 }
 
-exports.oldAvatar = (req, res) => {
-  const user = res.locals.user;
-  let { oldImages, image, email } = res.locals.user;
-  oldImages.remove(req.body.src);
-  oldImages.push(user.image);
-  image = req.body.src
-  User.findOneAndUpdate({ email }, { oldImages, image }, { new: true }, (err, user) => {
-    if (err)
-      return res.status(422).send({ errors: normalizeErrors(err.errors) });
-    if (user)
-      return res.status(200).json(user)
-  })
-}
+
+
 
 exports.auth = (req, res) => {
   const { email, password } = req.body;
@@ -82,8 +60,7 @@ exports.auth = (req, res) => {
     if (!user) {
       return res.status(404).send({ errors: { title: 'Người dùng không hợp lệ!', detail: 'Người dùng không tồn tại' } });
     }
-    if(!user.isVerified)
-     return res.status(401).send({ errors: { status: 'Error', detail: 'Tài khoản chưa xác thực' } });
+  
     if (user.hasSamePassword(password)) {
       const token = jwt.sign({
         userId: user.id,
@@ -101,181 +78,10 @@ exports.auth = (req, res) => {
 }
 
 
-exports.register = (req, res) => {
-  const { username, email, password, passwordConfirmation } = req.body;
-  if (!password || !email) {
-    return res.status(400).send({ errors: { status: 'Error', detail: 'Điền đầy đủ thông tin!' } });
-  }
-  if (password !== passwordConfirmation) {
-    return res.status(400).send({ errors: { status: 'Error', detail: 'Mật khẩu xác nhận không hợp lệ!' }});
-  }
-  User.findOne({ email }, (err, existingUser) => {
-    if (err) {
-      return res.status(422).send({ errors: normalizeErrors(err.errors) });
-    }
 
-    if (existingUser) {
-      return res.status(422).send({ errors: { status: 'Email không hợp lệ!', detail: 'Người dùng với email này đã tồn tại!' }});
-    }
-    User.findOne({ username }, (err, foundUser) => {
-      if (err) {
-        return res.status(422).send({ errors: normalizeErrors(err.errors) });
-      }
-      if (foundUser) {
-        return res.status(422).send({ errors: { status: 'Tên người dùng không hợp lệ!', detail: 'Người dùng với username này đã tồn tại!' }});
-      }
-      const registerToken = crypto.randomBytes(20).toString('hex');
-      const user = new User({
-        username,
-        email,
-        password,
-        registerToken
-      });
-      user.save((err) => {
-        if (err) {
-          return res.status(422).send({ errors: normalizeErrors(err.errors) });
-        }
-        var smtpTransport = nodemailer.createTransport(/*'SMTP',*/ {
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-          type: 'OAuth2',
-          user: '15520579@gm.uit.edu.vn',
-          clientId: '1084231637210-5s064d297c3enbsfhshjdiq74pjdab7a.apps.googleusercontent.com',
-          clientSecret: 'N_UDvg3p_N3B8A2tO8eCinLa',
-          refreshToken: '1/hYs8fnGIEHiBXMzz9m-VC5CWwAfsGQJb1q5yRTClkao',
-          accessToken: 'ya29.Gls7BrzkosyRcDTkCShI7GRG8hQ7aifSM4Cyr9W-BC8vehOrHI5vDW6hhzU-IPPa-uQMgZWq2urxJFnHlJE-01EA4ZNax6seEa_KLdY8xE7IMRBtMybk1PQ-uOUc'
-        }
-      });
-      var mailOptions = {
-        to: user.email,
-        from: 'registerconfirm@uitbooking.demo.com',
-        subject: 'Account Verification Token',
-        text: 'Please verify your account by clicking the link:\n\n' +
-          "localhost:3000/confirm/" + registerToken + '\n\n'       
-      };
-      smtpTransport.sendMail(mailOptions, (err) => {
-        if (err) 
-          return res.status(422).send({ errors: normalizeErrors(err.errors) });
-        return res.status(200).send({ result: { status: 'OK', detail:"Đăng ký thành công"}});
-      });
-        
-      })
-    })
-  }
-  )
-}
 
-exports.changePass = (req, res) => {
-  const { _id, password, newPassword } = req.body;
-  User.findOne({ _id }, (err, user) => {
-    if (err) {
-      return res.status(422).send({ errors: normalizeErrors(err.errors) });
-    }
-    if (!user) {
-      return res.status(422).send({ errors: [{ title: 'Người dùng không hợp lệ!', detail: 'Người dùng không tồn tại' }] });
-    }
 
-    if (user.hasSamePassword(password)) {
-      user.password = newPassword;
-      user.save((err) => {
-        if (err) {
-          return res.status(422).send({ errors: normalizeErrors(err.errors) });
-        }
-        return res.json({ 'changed': true });
-      })
-    }
-    else {
-      return res.status(422).send({ errors: [{ title: 'Sai dữ liệu!', detail: 'Sai mật khẩu' }] });
-    }
-  })
-}
-
-exports.resetPassword = (req, res) => {
-  async.waterfall([
-    (done) => {
-      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, (err, user) => {
-        if (!user) {
-          return res.status(401).send({ errors: [{ title: 'Token reset mật khẩu không hợp lệ hoặc hết hiệu lực' }] })
-        }
-        if (user) {
-          if (req.body.newPassword !== req.body.newPasswordConfirmation) {
-            return res.status(422).send({ errors: [{ title: 'Mật khẩu không hợp lệ!', detail: 'Mật khẩu xác nhận không hợp lệ!' }] });
-          }
-          user.password = req.body.newPassword;
-          user.resetPasswordToken = undefined;
-          user.resetPasswordExpires = undefined;
-        }
-        user.save((err) => {
-          if (err) {
-            return res.status(422).send({ errors: normalizeErrors(err.errors) });
-          }
-          return res.json({ 'reset': true });
-        });
-      });
-    },], (err) => {
-      return res.status(422).send({ errors: normalizeErrors(err.errors) });
-    });
-}
-
-exports.sendMailToken = (req, res, next) => {
-  async.waterfall([
-    (done) => {
-      crypto.randomBytes(20, (err, buf) => {
-        var token = buf.toString('hex');
-        done(err, token);
-      });
-    },
-    (token, done) => {
-      User.findOne({ email: req.body.email }, (err, user) => {
-        if (!user) {
-          return res.status(422).send({ errors: [{ title: 'Người dùng không hợp lệ!', detail: 'Người dùng không tồn tại' }] });
-        }
-        user.resetPasswordToken = token;
-        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-
-        user.save((err) => {
-          done(err, token, user);
-        });
-      });
-    },
-    (token, user, done) => {
-      var smtpTransport = nodemailer.createTransport(/*'SMTP',*/ {
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-          type: 'OAuth2',
-          user: '15520579@gm.uit.edu.vn',
-          clientId: '1084231637210-5s064d297c3enbsfhshjdiq74pjdab7a.apps.googleusercontent.com',
-          clientSecret: 'N_UDvg3p_N3B8A2tO8eCinLa',
-          refreshToken: '1/hYs8fnGIEHiBXMzz9m-VC5CWwAfsGQJb1q5yRTClkao',
-          accessToken: 'ya29.Gls7BrzkosyRcDTkCShI7GRG8hQ7aifSM4Cyr9W-BC8vehOrHI5vDW6hhzU-IPPa-uQMgZWq2urxJFnHlJE-01EA4ZNax6seEa_KLdY8xE7IMRBtMybk1PQ-uOUc'
-        }
-      });
-      var mailOptions = {
-        to: user.email,
-        from: 'passwordreset@uitbooking.demo.com',
-        subject: 'UIT Booking Password Reset',
-        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-          'http://localhost:3000/reset/' + token + '\n\n' +
-          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-      };
-      smtpTransport.sendMail(mailOptions, (err) => {
-        // req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
-        res.json({ 'sendSuccess': true });
-        done(err, 'done');
-      });
-    }
-  ], (err) => {
-    if (err) return next(err);
-    return res.status(422).send({ errors: normalizeErrors(err.errors) });
-  });
-}
-
-exports.authMiddleware = (req, res, next) => {
+exports.isAdmin = (req, res, next) => {
   const token = req.headers.authorization;
   
   if (token) {
@@ -286,41 +92,25 @@ exports.authMiddleware = (req, res, next) => {
         return res.status(422).send({ errors: normalizeErrors(err.errors) });
       }
 
-      if (user) {
+      if (user && user.role==='admin'){
         res.locals.user = user;
         next();
       } else {
-        return notAuthorized(res);
+        return res.status(401).send({ errors: { title: 'Không được chứng thực!', detail: 'Ban khong phai ADMIN!' } });
+
       }
     })
   } else {
     return notAuthorized(res);
   }
 }
-exports.authOrNot = (req, res, next) => {
-  const token = req.headers.authorization;
-  if (token) {
-    const user = parseToken(token);
 
-    User.findById(user.userId, (err, user) => {
-      if (err) {
-        return res.status(422).send({ errors: normalizeErrors(err.errors) });
-      }
-
-      if (user) {
-        res.locals.user = user;
-        next();
-      }
-    })
-  }
-  else next()
-}
 function parseToken(token) {
   return jwt.verify(token.split(' ')[1], config.SECRET);
 }
 
 function notAuthorized(res) {
-  return res.status(401).send({ errors: [{ title: 'Không được chứng thực!', detail: 'Bạn cần phải đăng nhập!' }] });
+  return res.status(401).send({ errors: { title: 'Không được chứng thực!', detail: 'Bạn cần phải đăng nhập!' } });
 }
 
 exports.updateInfo = (req, res) => {
@@ -345,33 +135,3 @@ exports.updateInfo = (req, res) => {
   })
 }
 
-exports.confirmation = (req,res) =>{
-  console.log(req.params.id);
-  User.findOneAndUpdate({ registerToken: req.params.id }, {isVerified: true, registerToken: undefined}, { new: true }, (err, user)=>{
-    if(err)
-          return res.status(422).send({ errors: normalizeErrors(err.errors) });    
-    if(!user)
-      return res.status(404).send({ errors:{ status: 'Error', detail: 'Token không hợp lệ hoặc tài khoản đã được xác nhận' }});
-    return res.status(200).send({result :{status:"OK",detail:"Đã xác nhận tài khoản thành công"}})
-  })
-}
-// exports.addSearchHistory = (req, res) => {
-//   const key = req.body.key
-//   const user = res.locals.user;  const _id = user.id
-
-//   const searchHistory = user.searchHistory;
-//   for (var i = 0; i < searchHistory.length; i++) {
-//     if (searchHistory[i] === key) {
-//       searchHistory.splice(i, 1);
-//     }
-//   }
-//   if(key!=null)
-//     searchHistory.unshift(key)
-//   User.findByIdAndUpdate({_id}, {searchHistory},{ new: true }, (err,user)=>{
-//     if(err)
-//     return res.status(422).send({ errors: normalizeErrors(err.errors) });
-//     if(!user)
-//       return res.status(422).send({ errors:{ title: 'Người dùng không hợp lệ!', detail: 'Người dùng không tồn tại' }});
-//   })
-
-// }

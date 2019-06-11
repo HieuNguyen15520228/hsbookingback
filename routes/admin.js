75372b02f4e136099d6688503ef7b0b745e8f915
Glitch (hsbookingbackend) <none>
2019-06-11 07:56:51 +0000

    🌈👩‍❤️‍💋‍👩 Checkpoint ./routes/admin.js:3622401/136

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 const AdminCtrl = require('../controllers/admin-user');
 const BookingCtrl = require('../controllers/admin-booking');
-const RentalCtrl = require('../controllers/admin-rental')
+const RentalCtrl = require('../controllers/admin-rental');
+const BlogCtrl = require('../controllers/admin-rental')
+
 router.post('/login', AdminCtrl.auth );
 router.get('/getUsers', AdminCtrl.isAdmin,AdminCtrl.getAllUser);
 router.post('/deactivate', AdminCtrl.isAdmin,AdminCtrl.deactivateUser);
@@ -13,5 +15,7 @@ router.get('/getRentals', AdminCtrl.isAdmin,RentalCtrl.getRentals);
 router.get('/getPendingRentals', AdminCtrl.isAdmin, RentalCtrl.getPendingRentals);
 router.post('/approveRental', AdminCtrl.isAdmin, RentalCtrl.approveRental);
 router.post('/forbidRental', AdminCtrl.isAdmin, RentalCtrl.forbidRental);
-router.get('/getNumbers', AdminCtrl.isAdmin, AdminCtrl.getNumbers)
+router.get('/getNumbers', AdminCtrl.isAdmin, AdminCtrl.getNumbers);
+
+router.get('/getPendingBlogs', AdminCtrl.isAdmin, )
 module.exports = router;

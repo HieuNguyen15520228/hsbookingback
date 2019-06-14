97f391afdffac7b5bc1192bfe6020ce2856c521d
Glitch (hsbookingbackend) <none>
2019-06-14 03:12:40 +0000

    🍰👜 Checkpoint ./routes/comments.js:3622401/34

--- routes/comments.js
+++ routes/comments.js
@@ -3,11 +3,10 @@ const router = express.Router();
 
 const UserCtrl = require('../controllers/user');
 const CommentCtrl = require('../controllers/comment');
-
 router.post("/post", UserCtrl.authMiddleware, CommentCtrl.postComment)
 router.post("/get", CommentCtrl.getComment);
-router.delete("/:id", CommentCtrl.removeOneComment)
-router.post('/createContact',)
+router.delete("/:id", CommentCtrl.removeOneComment);
+router.post('/createContact',CommentCtrl.createContact);
 module.exports = router;
 
 
